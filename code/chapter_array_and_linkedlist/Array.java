412c1c9b056207cbe282e4da8b68194d820b37b9
harkjeans <chengqi172@gmail.com>
2025-10-06 09:29:29 +0800

    Array

--- code/chapter_array_and_linkedlist/Array.java
+++ code/chapter_array_and_linkedlist/Array.java
@@ -13,8 +13,8 @@ public class Array {
         return randomNum;
     }
 
-      /* 数组扩容 */
-    static int[] extend(int[] nums, int enlarge){
+    /* 数组扩容 */
+    static int[] extend(int[] nums, int enlarge) {
         // 初始化一个扩展长度后的数组
         int[] res = new int[nums.length + enlarge];
         // 将原数组中的所有元素复制到新数组
@@ -23,6 +23,47 @@ public class Array {
         }
         // 返回扩展后的新数组
         return res;
+    }
+
+    /* 在数组索引 index 处插入元素 num */
+    static void insert(int[] nums, int num, int index){
+        // 把索引 index 以及之后的所有元素向后移动一位
+        for (int i = nums.length - 1; i > index; i--) {
+            nums[i] = nums[i - 1];
+        }
+        // 将 num 赋给 index 处的元素
+        nums[index] = num;
+    }
+    
+    /* 删除索引 index 处的元素 */
+    static void remove(int[] nums, int index) {
+        // 把索引 index 之后的所有元素向前移动一位
+        for (int i = index; i < nums.length - 1; i++) {
+            nums[i] = nums[i + 1];
+        }
+    }       
+       
+    /* 遍历数组 */    
+    static void traverse(int[] nums) {
+        // 通过索引遍历数组
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+        // 直接遍历数组元素
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+    
+    /* 在数组中查找指定元素 */
+    static int find(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 
@@ -42,9 +83,19 @@ public class Array {
         System.out.println("将数组长度扩展至8,得到nums = " + Arrays.toString(nums));
 
         /* 插入元素 */
-        
+        insert(nums, 6, 3);
+        System.out.println("在索引3处插入数字6,得到nums = " + Arrays.toString(nums));
 
+        /* 删除元素 */
+        remove(nums, 2);
+        System.out.println("删除索引2处的元素, 得到 nums = " + Arrays.toString(nums));
 
+        /* 遍历数组 */
+        traverse(nums);
+
+        /* 查找元素 */
+        int index = find(nums, 3);
+        System.out.println("在nums中查找元素 3 得到对应索引 = " + index);
     }
    
 }
